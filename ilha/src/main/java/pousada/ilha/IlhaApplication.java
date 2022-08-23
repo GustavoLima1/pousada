2863b91e59be26d368b2eb8e855e901eb9e4e2d6
Gustavo Lima de Souza <g.lima.de.souza@avanade.com>
2022-08-22 22:05:59 -0300

    Meu primeiro commit

---
+++ ilha/src/main/java/pousada/ilha/IlhaApplication.java
@@ -0,0 +1,13 @@
+package pousada.ilha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlhaApplication.class, args);
+	}
+
+}
---
+++ ilha/src/test/java/pousada/ilha/IlhaApplicationTests.java
@@ -0,0 +1,13 @@
+package pousada.ilha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
