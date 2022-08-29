1dea06289b2caf7e8b9333c685e88441c31ed0ce
Gustavo Lima de Souza <g.lima.de.souza@avanade.com>
2022-08-29 20:24:51 -0300

    conexão com o banco de dados

--- ilha/src/main/java/pousada/ilha/IlhaApplication.java
+++ ilha/src/main/java/pousada/ilha/IlhaApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IlhaApplication {
-
-	public static void main(String[] args) {
+	
+	public static void main(String...args) {
 		SpringApplication.run(IlhaApplication.class, args);
 	}
 
